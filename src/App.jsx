372c8c6cbab3c16bd2a8e6a6ed2ad75252e11d7c
BritishErrorCoins <jrenaud001@gmail.com>
2025-05-30 21:30:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
-// TODO: Implement App.jsx
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Coin Collection App</h1>
+      <p>The app is running correctly.</p>
+    </div>
+  );
+}
+
+export default App;
