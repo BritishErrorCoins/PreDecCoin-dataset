ff1bfb65e4180ad202d49be99d17a655c0a43cf2
BritishErrorCoins <jrenaud001@gmail.com>
2025-05-30 20:50:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // optional, if you use styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-// TODO: Implement index.js
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
